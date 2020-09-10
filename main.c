cbda4038c5ab3896109f28ea8b4048f22ea00354
Andrew Palmer <atp@sdf.org>
2020-09-10 13:20:38 -0500

    Break out test functions

--- main.c
+++ main.c
@@ -7,14 +7,30 @@
 
 #define HTTP_AUTH_BASIC_INIT(user, password)        {{HTTP_AUTHTYPE_BASIC}, (user), (password)}
 
-int main(void) {
-    struct http_auth_basic auth = HTTP_AUTH_BASIC_INIT("me", "p4$$w0rd");
+void cookies(void) {
     struct http_opts opts = {
         .cookies = "key1=value1;key2=value2",
         .timeout_secs = 1,
+    };
+    struct http_response *resp = http_request("GET", HTTPBIN_URL_COOKIES, &opts);
+    if(!resp) {
+        printf("Error Code: %d\n", http_curl_error_code);
+        printf("Error Text: %s\n", http_curl_error_text());
+    }
+    printf("CONTENT (%lu):\n%s\n\n", http_response_content_length(resp), http_response_content(resp));
+    printf("HEADERS (%lu):\n%s\n\n", http_response_headers_length(resp), http_response_headers(resp));
+    printf("URL: %s\n", http_response_url(resp));
+    printf("Status Code: %d\n", http_response_status_code(resp));
+    printf("Elapsed Seconds: %f\n", http_response_elapsed(resp));
+    http_response_free(resp);
+}
+
+void auth_basic(void) {
+    struct http_auth_basic auth = HTTP_AUTH_BASIC_INIT("me", "p4$$w0rd");
+    struct http_opts opts = {
+        .timeout_secs = 1,
         .auth = (struct http_auth *)&auth,
     };
-    /* struct http_response *resp = http_request("GET", HTTPBIN_URL_COOKIES, &opts); */
     struct http_response *resp = http_request("GET", HTTPBIN_URL_BASIC_AUTH("me", "p4$$w0rd"), &opts);
     if(!resp) {
         printf("Error Code: %d\n", http_curl_error_code);
@@ -26,4 +42,9 @@ int main(void) {
     printf("Status Code: %d\n", http_response_status_code(resp));
     printf("Elapsed Seconds: %f\n", http_response_elapsed(resp));
     http_response_free(resp);
+}
+
+int main(void) {
+    /* cookies(); */
+    auth_basic();
 }
