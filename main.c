eb7a8d8ed2fef5b239ac34070b3948056294058a
Andrew Palmer <atp@sdf.org>
2020-09-10 13:23:28 -0500

    Break out response_print function

--- main.c
+++ main.c
@@ -7,21 +7,27 @@
 
 #define HTTP_AUTH_BASIC_INIT(user, password)        {{HTTP_AUTHTYPE_BASIC}, (user), (password)}
 
+void response_print(struct http_response *resp) {
+    if(!resp) {
+        printf("Error Code: %d\n", http_curl_error_code);
+        printf("Error Text: %s\n", http_curl_error_text());
+        return;
+    }
+
+    printf("CONTENT (%lu):\n%s\n\n", http_response_content_length(resp), http_response_content(resp));
+    printf("HEADERS (%lu):\n%s\n\n", http_response_headers_length(resp), http_response_headers(resp));
+    printf("URL: %s\n", http_response_url(resp));
+    printf("Status Code: %d\n", http_response_status_code(resp));
+    printf("Elapsed Seconds: %f\n", http_response_elapsed(resp));
+}
+
 void cookies(void) {
     struct http_opts opts = {
         .cookies = "key1=value1;key2=value2",
         .timeout_secs = 1,
     };
     struct http_response *resp = http_request("GET", HTTPBIN_URL_COOKIES, &opts);
-    if(!resp) {
-        printf("Error Code: %d\n", http_curl_error_code);
-        printf("Error Text: %s\n", http_curl_error_text());
-    }
-    printf("CONTENT (%lu):\n%s\n\n", http_response_content_length(resp), http_response_content(resp));
-    printf("HEADERS (%lu):\n%s\n\n", http_response_headers_length(resp), http_response_headers(resp));
-    printf("URL: %s\n", http_response_url(resp));
-    printf("Status Code: %d\n", http_response_status_code(resp));
-    printf("Elapsed Seconds: %f\n", http_response_elapsed(resp));
+    response_print(resp);
     http_response_free(resp);
 }
 
@@ -32,15 +38,7 @@ void auth_basic(void) {
         .auth = (struct http_auth *)&auth,
     };
     struct http_response *resp = http_request("GET", HTTPBIN_URL_BASIC_AUTH("me", "p4$$w0rd"), &opts);
-    if(!resp) {
-        printf("Error Code: %d\n", http_curl_error_code);
-        printf("Error Text: %s\n", http_curl_error_text());
-    }
-    printf("CONTENT (%lu):\n%s\n\n", http_response_content_length(resp), http_response_content(resp));
-    printf("HEADERS (%lu):\n%s\n\n", http_response_headers_length(resp), http_response_headers(resp));
-    printf("URL: %s\n", http_response_url(resp));
-    printf("Status Code: %d\n", http_response_status_code(resp));
-    printf("Elapsed Seconds: %f\n", http_response_elapsed(resp));
+    response_print(resp);
     http_response_free(resp);
 }
 
